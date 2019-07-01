930e986e1087926afad4cb47f8ac123f385f93a5
Walter Kraxberger <wkrax@hotmail.com>
2019-07-01 12:09:43 -0300

    Changed Ruby version

--- app/models/Recipe.rb
+++ app/models/Recipe.rb
@@ -1,8 +1,8 @@
 class Recipe
   include HTTParty
 
-  key_value = ENV["FOOD2FORK_KEY"]
-  hostport = ENV["FOOD2FORK_SERVER_AND_PORT"] || 'www.food2fork.com'
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 
   base_uri "https://#{hostport}/api/search"
   default_params key: key_value
